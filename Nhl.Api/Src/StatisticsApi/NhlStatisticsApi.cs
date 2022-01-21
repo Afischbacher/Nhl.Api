954495f6feebea0369b57a56620ae884dbf2cbfd
ANDRE-PC\ital- <ital-andre95@live.com>
2022-01-21 12:51:54 -0500

    added comments and other updates

--- Nhl.Api/Src/StatisticsApi/NhlStatisticsApi.cs
+++ Nhl.Api/Src/StatisticsApi/NhlStatisticsApi.cs
@@ -298,11 +298,25 @@ namespace Nhl.Api
                 .SingleOrDefault();
         }
 
+        /// <summary>
+        /// Returns the player with the top NHL player statistic based on the selected season year
+        /// </summary>
+        /// <param name="seasonYear">The argument for the NHL season of the play, see <see cref="SeasonYear"/> for more information</param>
+        /// <param name="playerStatisticEnum">The argument for the type of NHL player statistic, see <see cref="PlayerStatisticEnum"/> for more information </param>
+        /// <param name="numberOfPlayers">The argument for the number of players to retrieve, default value is 10 </param>
+        /// <returns>Returns the player profile with the top player statistic in the specified NHL season</returns>
         public async Task<List<PlayerStatisticResult>> GetPlayersWithTopStatisticBySeasonAsync(PlayerStatisticEnum playerStatisticEnum, string seasonYear, int numberOfPlayers = 10)
         {
             return await GetPlayerStatisticResultsAsync(playerStatisticEnum, seasonYear, numberOfPlayers);
         }
 
+        /// <summary>
+        /// Returns the goalie with the top NHL goalie statistic based on the selected season year
+        /// </summary>
+        /// <param name="goalieStatisticEnum">The argument for the type of NHL goalie statistic, see <see cref="GoalieStatisticEnum"/> for more information </param>
+        /// <param name="seasonYear">The argument for the NHL season of the play, see <see cref="SeasonYear"/> for more information</param>
+        /// <param name="numberOfGoalies">The argument for the number of goalies to retrieve, default value is 10</param>
+        /// <returns>Returns the goalie profile with the top player statistic in the specified NHL season</returns>
         public async Task<List<GoalieStatisticResult>> GetGoaliesWithTopStatisticBySeasonAsync(GoalieStatisticEnum goalieStatisticEnum, string seasonYear, int numberOfGoalies = 10)
         {
             return await GetGoalieStatisticResultsAsync(goalieStatisticEnum, seasonYear, numberOfGoalies);
