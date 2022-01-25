c04366dcba6ae645292a04febb768aaa6a6105e6
ANDRE-PC\ital- <ital-andre95@live.com>
2022-01-24 22:11:52 -0500

    minor fix

--- Nhl.Api.Tests/StatisticsTests.cs
+++ Nhl.Api.Tests/StatisticsTests.cs
@@ -310,7 +310,7 @@ namespace Nhl.Api.Tests
             {
                 await _nhlPlayerStatisticsAsyncRetryPolicy.ExecuteAsync(async () =>
                 {
-                   var player = await nhlApi.GetPlayerByStatisticTypeBySeasonAsync(PlayerStatisticEnum.Goals, SeasonYear.season20192020);
+                   var player = await nhlApi.GetPlayerByStatisticTypeBySeasonAsync(playerStatistic, SeasonYear.season20192020);
 
                     Assert.IsNotNull(player);
                     Assert.IsNotNull(player.PlayerStatisticsData);
