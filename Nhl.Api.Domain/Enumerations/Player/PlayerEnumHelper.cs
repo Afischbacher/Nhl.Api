7f9d2d289b7b9da88c00e1d8fe331eec5a88e57d
ANDRE-PC\ital- <ital-andre95@live.com>
2022-10-15 19:50:15 -0400

    minor clean up

--- Nhl.Api.Domain/Enumerations/Player/PlayerEnumHelper.cs
+++ Nhl.Api.Domain/Enumerations/Player/PlayerEnumHelper.cs
@@ -16,7 +16,7 @@ namespace Nhl.Api.Models.Enumerations.Player
     public static class PlayerEnumHelper
     {
         /// <summary>
-        /// Retrieves all NHL players
+        /// Retrieves all NHL players to have player in the NHL
         /// </summary>
         /// <returns>A dictionary of players names and their identifiers for every NHL player to ever play</returns>
         public static Dictionary<string, int> GetAllPlayers()
@@ -60,48 +60,6 @@ namespace Nhl.Api.Models.Enumerations.Player
             }
 
             return internalPlayerEnumValuesDictionary;
-        }
-
-        /// <summary>
-        /// A helper method that returns every single NHL player known
-        /// </summary>
-        /// <returns>A comprehensive list of NHL players</returns>
-        public static async Task<Dictionary<string, int>> GetAllPlayersAsync()
-        {
-            //var nhlStatsApiHttpClient = new Api.Common.Http.NhlStatsApiHttpClient();
-            //var players = new ConcurrentBag<Models.Player.Player>();
-
-            //var internalPlayerEnumValues = typeof(InternalPlayerEnum)
-            //    .GetEnumValues()
-            //    .Cast<object>();
-
-            //var internalPlayerEnumValuesDictionary = internalPlayerEnumValues.ToDictionary(key => key.ToString(), value => (int)value);
-            //var lastEnumValue = (int)internalPlayerEnumValues.Last();
-
-            //Parallel.For(lastEnumValue, 8490000, new ParallelOptions { MaxDegreeOfParallelism = 16 }, async (i) =>
-            //{
-            //    try
-            //    {
-            //        var player = (await nhlStatsApiHttpClient.GetAsync<LeaguePlayers>($"/people/{i}"))
-            //        .Players
-            //        .SingleOrDefault();
-
-            //        players.Add(player);
-            //    }
-            //    finally
-            //    {
-            //    }
-
-            //});
-
-            //foreach (var player in players)
-            //{
-            //    internalPlayerEnumValuesDictionary.Add(Regex.Replace(player.FullName, @"('|\.|\s|-|_|&|)", ""), player.Id);
-            //}
-
-            //return internalPlayerEnumValuesDictionary;
-
-            return new Dictionary<string, int>();
         }
     }
 }
