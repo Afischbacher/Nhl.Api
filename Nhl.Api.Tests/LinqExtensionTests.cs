5759edbdd13943c5a3bbcadd6246fddacaf4643f
ANDRE-PC\ital- <ital-andre95@live.com>
2022-11-03 23:15:07 -0400

    added more test cases

---
+++ Nhl.Api.Tests/LinqExtensionTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhl.Api.Tests
+{
+
+    [TestClass]
+    public class LinqExtensionTests
+    {
+        class TestLinqClass
+        {
+            public int Number { get; set; }
+        }
+
+        [TestMethod]   
+        public void TestDistinctByLinqMethod() 
+        {
+            var testCollection = new List<TestLinqClass>()
+            {
+               new TestLinqClass{ Number = 1 },
+               new TestLinqClass{ Number = 2 },
+               new TestLinqClass{ Number = 3 },
+               new TestLinqClass{ Number = 2 }
+            };
+
+            var distinctValues = testCollection.DistinctBy(c => c.Number);
+
+            Assert.IsNotNull(distinctValues);
+            Assert.AreEqual(3, distinctValues.Count());
+        }
+    }
+}
