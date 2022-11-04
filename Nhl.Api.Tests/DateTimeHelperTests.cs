387f4dd1322bced87edf5bbb6ca1a8688a40e0d3
ANDRE-PC\ital- <ital-andre95@live.com>
2022-11-03 23:06:38 -0400

    added tests for helpers

---
+++ Nhl.Api.Tests/DateTimeHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nhl.Api.Common.Helpers;
+
+namespace Nhl.Api.Tests
+{
+
+    [TestClass]
+    public class DateTimeHelperTests
+    {
+
+        [DataRow("20211105_201423")]
+        [DataRow("20201015_201423")]
+        [DataRow("19950210_201423")]
+        [TestMethod]
+        public void TestDateTimeOffsetHelper(string dateTimeString)
+        {
+            // Act/Arrange
+            var dateTimeOffset = TimeStampHelper.ParseTimeStampToDateTimeOffset(dateTimeString);
+
+            // Assert
+            Assert.IsNotNull(dateTimeOffset);
+        }
+    }
+}
