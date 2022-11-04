cacf3dc834efb2ca833aa2813eaa2ef86527912f
ANDRE-PC\ital- <ital-andre95@live.com>
2022-11-03 23:11:10 -0400

    more cases

--- Nhl.Api.Tests/DateTimeHelperTests.cs
+++ Nhl.Api.Tests/DateTimeHelperTests.cs
@@ -12,13 +12,28 @@ namespace Nhl.Api.Tests
         [DataRow("20201015_201423")]
         [DataRow("19950210_201423")]
         [TestMethod]
-        public void TestDateTimeOffsetHelper(string dateTimeString)
+        public void TestValidDateTimeOffsetHelper(string dateTimeString)
         {
             // Act/Arrange
             var dateTimeOffset = TimeStampHelper.ParseTimeStampToDateTimeOffset(dateTimeString);
 
             // Assert
             Assert.IsNotNull(dateTimeOffset);
+        }
+
+        [DataRow("20211105_201423232367644")]
+        [DataRow("20201015_201424345643")]
+        [DataRow("19950210_20142423234543")]
+        [DataRow(" ")]
+        [DataRow("19950210_201424232_34543")]
+        [TestMethod]
+        public void TestInValidDateTimeOffsetHelper(string dateTimeString)
+        {
+            // Act/Arrange
+            var dateTimeOffset = TimeStampHelper.ParseTimeStampToDateTimeOffset(dateTimeString);
+
+            // Assert
+            Assert.IsNull(dateTimeOffset);
         }
     }
 }
