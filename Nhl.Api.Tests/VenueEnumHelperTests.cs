50102894aed98ee4aaaf26caa7b4d332e04848aa
ANDRE-PC\ital- <ital-andre95@live.com>
2022-11-05 12:38:56 -0400

    added helper tests

---
+++ Nhl.Api.Tests/VenueEnumHelperTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nhl.Api.Models.Enumerations.Team;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhl.Api.Tests
+{
+
+    [TestClass]
+    public class VenueEnumHelperTests
+    {
+        [TestMethod]
+        public void GetAllVenuesTest() 
+        {
+            var venues = VenueEnumHelper.GetAllVenues();
+            Assert.IsNotNull(venues);
+            Assert.AreEqual(31, venues.Count);
+        }
+    }
+}
