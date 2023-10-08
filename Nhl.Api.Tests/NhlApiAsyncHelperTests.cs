33c29be42f58b0261ea9ade1ef9e8e4f9da9b4db
ANDRE-PC\ital- <ital-andre95@live.com>
2023-10-08 17:39:32 -0400

    spacing

--- Nhl.Api.Tests/NhlApiAsyncHelperTests.cs
+++ Nhl.Api.Tests/NhlApiAsyncHelperTests.cs
@@ -11,27 +11,27 @@ namespace Nhl.Api.Tests
     public class NhlApiAsyncHelperTests
     {
         [TestMethodWithRetry(RetryCount = 5)]
-        public async Task TestRunSyncAsync() 
+        public async Task TestRunSyncAsync()
         {
-            NhlApiAsyncHelper.RunSync(async () =>  await Task.Run(() => Task.Delay(100)) );
+            NhlApiAsyncHelper.RunSync(async () => await Task.Run(() => Task.Delay(100)));
         }
 
         [TestMethodWithRetry(RetryCount = 5)]
         public async Task TestForEachAsync()
         {
-            var numbers = new ConcurrentBag<int>(new [] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, });
+            var numbers = new ConcurrentBag<int>(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 });
 
             var sumOfNumbers = numbers.Sum();
 
             // Arrange + Act 
-            await NhlApiAsyncHelper.ForEachAsync<int>(numbers, 2, async(i) =>
+            await NhlApiAsyncHelper.ForEachAsync<int>(numbers, 2, async (i) =>
             {
                 var value = numbers.TryTake(out i);
                 numbers.Add(i * 2);
             });
 
             Assert.AreNotEqual(numbers.Sum(), sumOfNumbers);
-            
+
         }
     }
 }
