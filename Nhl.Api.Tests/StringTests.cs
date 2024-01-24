f13c17bc1c14fb54665634f083b1aaba61fc6448
ANDRE-PC\ital- <ital-andre95@live.com>
2024-01-23 22:09:14 -0500

    more tests

--- Nhl.Api.Tests/StringTests.cs
+++ Nhl.Api.Tests/StringTests.cs
@@ -9,7 +9,14 @@ public class StringTests
     [TestMethod]
     [DataRow("ŠĐĆŽćžšđ", "SDCZczsd")]
     [DataRow("àndre", "andre")]
-
+    [DataRow("Héllo", "Hello")]
+    [DataRow("Café", "Cafe")]
+    [DataRow("Nöël", "Noel")]
+    [DataRow("âpple", "apple")]
+    [DataRow("böök", "book")]
+    [DataRow("mønitor", "monitor")]
+    [DataRow("éléphant", "elephant")]
+    [DataRow("wôrld", "world")]
     public void ReplaceNonAsciiWithAscii_Returns_Correct_Ascii_String(string input, string expected)
     {
         var actual = PlayerEnumFileGeneratorHelper.ReplaceNonAsciiWithAscii(input);
