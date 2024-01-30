eca7e9a549592e2f4df475c2ad4b6e5669b5da12
ANDRE-PC\ital- <ital-andre95@live.com>
2024-01-29 22:21:21 -0500

    added more leagues

--- Nhl.Api.Common/Helpers/LeagueHelper.cs
+++ Nhl.Api.Common/Helpers/LeagueHelper.cs
@@ -1,49 +1,132 @@
-﻿
-namespace Nhl.Api.Common.Helpers
+﻿namespace Nhl.Api.Common.Helpers;
+
+/// <summary>
+/// The league helper class for all league related information
+/// </summary>
+public static class LeagueHelper
 {
     /// <summary>
-    /// The league helper class for all league related information
+    /// The National Hockey League
     /// </summary>
-    public static class LeagueHelper
-    {
-        /// <summary>
-        /// The National Hockey League
-        /// </summary>
-        public const string NationalHockeyLeague = "NHL";
+    public const string NationalHockeyLeague = "NHL";
 
-        /// <summary>
-        /// The American Hockey League
-        /// </summary>
-        public const string AmericanHockeyLeague = "AHL";
+    /// <summary>
+    /// The American Hockey League
+    /// </summary>
+    public const string AmericanHockeyLeague = "AHL";
 
-        /// <summary>
-        /// The East Coast Hockey League
-        /// </summary>
-        public const string EastCoastHockeyLeague = "ECHL";
+    /// <summary>
+    /// The East Coast Hockey League
+    /// </summary>
+    public const string EastCoastHockeyLeague = "ECHL";
 
-        /// <summary>
-        /// The Ontario Hockey League
-        /// </summary>
-        public const string OntarioHockeyLeague = "OHL";
+    /// <summary>
+    /// The Ontario Hockey League
+    /// </summary>
+    public const string OntarioHockeyLeague = "OHL";
 
-        /// <summary>
-        /// The Western Hockey League
-        /// </summary>
-        public const string WesternHockeyLeague = "WHL";
+    /// <summary>
+    /// The Western Hockey League
+    /// </summary>
+    public const string WesternHockeyLeague = "WHL";
 
-        /// <summary>
-        /// The Quebec Major Junior Hockey League
-        /// </summary>
-        public const string QuebecMajorJuniorHockeyLeague = "QMJHL";
+    /// <summary>
+    /// The Quebec Major Junior Hockey League
+    /// </summary>
+    public const string QuebecMajorJuniorHockeyLeague = "QMJHL";
 
-        /// <summary>
-        /// The National Womens Hockey League
-        /// </summary>
-        public const string NationalWomensHockeyLeague = "NWHL";
+    /// <summary>
+    /// Kontinental Hockey League (Russia, Belarus, China, Finland, Kazakhstan, Latvia, Slovakia)
+    /// </summary>
+    public const string KontinentalHockeyLeague = "KHL";
 
-        /// <summary>
-        /// The Canadian Womens Hockey League
-        /// </summary>
-        public const string CanadianWomensHockeyLeague = "CWHL";
-    }
+    /// <summary>
+    /// Swedish Hockey League (Sweden)
+    /// </summary>
+    public const string SwedishHockeyLeague = "SHL";
+
+    /// <summary>
+    /// Liiga (Finland)
+    /// </summary>
+    public const string Liiga = "Liiga";
+
+    /// <summary>
+    /// National League (Switzerland)
+    /// </summary>
+    public const string NationalLeague = "NL";
+
+    /// <summary>
+    /// Czech Extraliga (Czech Republic)
+    /// </summary>
+    public const string CzechExtraliga = "CEL";
+
+    /// <summary>
+    /// Deutsche Eishockey Liga (Germany)
+    /// </summary>
+    public const string DeutscheEishockeyLiga = "DEL";
+
+    /// <summary>
+    /// Elite Ice Hockey League (United Kingdom)
+    /// </summary>
+    public const string EliteIceHockeyLeague = "EIHL";
+
+    /// <summary>
+    /// Australian Ice Hockey League (Australia)
+    /// </summary>
+    public const string AustralianIceHockeyLeague = "AIHL";
+
+    /// <summary>
+    /// Asia League Ice Hockey (Japan, South Korea, China, Russia)
+    /// </summary>
+    public const string AsiaLeagueIceHockey = "ALIH";
+
+    /// <summary>
+    /// Slovak Extraliga (Slovakia)
+    /// </summary>
+    public const string SlovakExtraliga = "SlovakExtraliga";
+
+    /// <summary>
+    /// Danish Metal Ligaen (Denmark)
+    /// </summary>
+    public const string DanishMetalLigaen = "DanishMetalLigaen";
+
+    /// <summary>
+    /// GET-ligaen (Norway)
+    /// </summary>
+    public const string GETLigaen = "GET-ligaen";
+
+    /// <summary>
+    /// Erste Bank Eishockey Liga (Austria, Hungary, Czech Republic, Italy, Slovenia)
+    /// </summary>
+    public const string ErsteBankEishockeyLiga = "EBEL";
+
+    /// <summary>
+    /// French Ligue Magnus (France)
+    /// </summary>
+    public const string FrenchLigueMagnus = "LigueMagnus";
+
+    /// <summary>
+    /// Polish Hockey League (Poland)
+    /// </summary>
+    public const string PolishHockeyLeague = "PHL";
+
+    /// <summary>
+    /// Belarusian Extraleague (Belarus)
+    /// </summary>
+    public const string BelarusianExtraleague = "BelarusianExtraleague";
+
+    /// <summary>
+    /// Slovak 1. Liga (Slovakia - Second-tier league)
+    /// </summary>
+    public const string Slovak1Liga = "Slovak1Liga";
+
+    /// <summary>
+    /// The National Womens Hockey League
+    /// </summary>
+    public const string NationalWomensHockeyLeague = "NWHL";
+
+    /// <summary>
+    /// The Canadian Womens Hockey League
+    /// </summary>
+    public const string CanadianWomensHockeyLeague = "CWHL";
 }
