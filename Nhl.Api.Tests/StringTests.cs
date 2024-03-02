f9bc3d07298c3aab6069d86989c8b6c1100c4380
ANDRE-PC\ital- <ital-andre95@live.com>
2024-03-02 12:37:45 -0500

    added tests for string

--- Nhl.Api.Tests/StringTests.cs
+++ Nhl.Api.Tests/StringTests.cs
@@ -40,4 +40,73 @@ public class StringTests
     {
         Assert.AreEqual(input.ReplaceNonAsciiWithAscii(), expected);
     }
+
+    [TestMethod]
+    [DataRow("ÀÁÂÃÄÅ", "AAAAAA")]
+    [DataRow("Æ", "AE")]
+    [DataRow("ÇĆ", "CC")]
+    [DataRow("ÈÉÊË", "EEEE")]
+    [DataRow("ÌÍÎÏ", "IIII")]
+    [DataRow("ÐĐ", "DD")]
+    [DataRow("Ñ", "N")]
+    [DataRow("ÒÓÔÕÖ", "OOOOO")]
+    [DataRow("Ø", "O")]
+    [DataRow("ÙÚÛÜ", "UUUU")]
+    [DataRow("Š", "S")]
+    [DataRow("ÝŸ", "YY")]
+    [DataRow("Ž", "Z")]
+    [DataRow("àáâãä", "aaaaa")]
+    [DataRow("å", "a")]
+    [DataRow("æ", "ae")]
+    [DataRow("çć", "cc")]
+    [DataRow("đ", "d")]
+    [DataRow("èéêë", "eeee")]
+    [DataRow("ìíîï", "iiii")]
+    [DataRow("ð", "d")]
+    [DataRow("ñ", "n")]
+    [DataRow("òóôõö", "ooooo")]
+    [DataRow("ø", "o")]
+    [DataRow("ùúûü", "uuuu")]
+    [DataRow("š", "s")]
+    [DataRow("ýÿ", "yy")]
+    [DataRow("ž", "z")]
+    public void ReplaceNonAsciiWithAscii_Test(string input, string expected)
+    {
+        // Arrange
+        // No need for arrange as we have input and expected values
+
+        // Act
+        var result = input.ReplaceNonAsciiWithAscii();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void ReplaceNonAsciiWithAscii_WithEmptyString_Test()
+    {
+        // Arrange
+        var input = "";
+        var expected = "";
+
+        // Act
+        string result = input.ReplaceNonAsciiWithAscii();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void ReplaceNonAsciiWithAscii_WithOnlyAsciiCharacters_Test()
+    {
+        // Arrange
+        var input = "HelloWorld";
+        var expected = "HelloWorld";
+
+        // Act
+        string result = input.ReplaceNonAsciiWithAscii();
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
 }
