8c4830a6eb7dd6424b4a082799c297f04487450a
ANDRE-PC\ital- <ital-andre95@live.com>
2024-03-05 18:29:40 -0500

    added project tests

---
+++ Nhl.Api.Tests/ProjectStructureTests.cs
@@ -0,0 +1,52 @@
+﻿
+using System.IO;
+
+namespace Nhl.Api.Tests;
+
+[TestClass]
+public class ProjectStructureTests
+{
+
+    #region constants
+    private const string Nhl = "Nhl";
+    #endregion
+
+    [TestMethod]
+    public void ValidateProjectStructure_Should_Be_Always_Contain_Nhl()
+    {
+
+        var getSolutionFile = RootDirectoryFolder.GetFiles("*.sln", SearchOption.AllDirectories);
+        Assert.IsTrue(getSolutionFile.Length > 0);
+
+        var projectFiles = RootDirectoryFolder.GetFiles("*.csproj", SearchOption.AllDirectories);
+
+        CollectionAssert.AllItemsAreNotNull(projectFiles);
+        
+        foreach (var projectFile in projectFiles)
+        {
+            Assert.IsTrue(projectFile.Name.Contains(Nhl));
+            Assert.IsTrue(projectFile.Length > 0);
+        }
+    }
+
+    [TestMethod]
+    public void ValidateProjectStructure_Should_Never_Have_Domain_In_Namespace()
+    {
+        var getSourceCSharpFiles = RootDirectoryFolder.GetFiles("*.cs", SearchOption.AllDirectories);
+        foreach (var sourceCSharpFile in getSourceCSharpFiles)
+        {
+            if (sourceCSharpFile.FullName.Contains("Tests"))
+            {
+                continue;
+            }   
+
+            var sourceCode = File.ReadAllText(sourceCSharpFile.FullName);
+            Assert.IsFalse(sourceCode.Contains("namespace Nhl.Api.Domain"));
+        }
+    }
+
+    #region Private Methods
+    private DirectoryInfo RootDirectoryFolder => Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent;
+
+    #endregion
+}
