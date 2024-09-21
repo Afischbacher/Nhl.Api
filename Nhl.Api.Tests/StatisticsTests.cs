b3381ccef58bdaaa8cd90b715ccbefd392748802
Andre Fischbacher <ital-andre95@live.com>
2024-09-21 15:14:50 -0400

    Update Nhl.Api.Tests/StatisticsTests.cs
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Nhl.Api.Tests/StatisticsTests.cs
+++ Nhl.Api.Tests/StatisticsTests.cs
@@ -671,7 +671,7 @@ public class StatisticsTests
 
         var result = await nhlApi.GetRealtimePlayerStatisticsBySeasonAndFilterExpressionAsync(SeasonYear.season20202021, expression);
 
-        // Act
+        // Assert
         Assert.IsNotNull(expression);
         Assert.IsNotNull(result);
         Assert.IsTrue(result.Total >= 300);
